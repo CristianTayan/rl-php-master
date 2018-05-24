8337dc8b676678becf68e97ad9f150b4f1bea914
Cristian Tayan <cris17j@gmail.com>
2018-05-24 08:37:46 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -3,6 +3,6 @@ $host="localhost";
 $user="root";
 $password="";
 $db="myapp";
-$con = new mysqli($host,$user,$password,$db);
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "{smileexamen.mysql.database.azure.com}", "{CristianTayan@smileexamen}", {1991Cris}, {myapp}, 3306);
 
 ?>
