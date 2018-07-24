ae04cc78c3740aa40f3d6a0c9bb2373ffa475392
Cristian Tayan <cris17j@gmail.com>
2018-07-24 13:09:17 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php
-$host="smileexamen.mysql.database.azure.com";
-$user="CristianTayan@smileexamen";
+$host="proyectofinalcloud.mysql.database.azure.com";
+$user="CristianTayan@proyectofinalcloud";
 $password="1991Cris";
 $db="myapp";
 $con = new mysqli($host,$user,$password,$db);
